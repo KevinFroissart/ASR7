1e38a8fd656d5a6a8edfbe981c248665bebbad22
Froissart Kevin <kevin.froissart69@gmail.com>
2021-01-26 09:30:51 +0100

    TP1

--- lancement.cpp
+++ lancement.cpp
@@ -3,43 +3,50 @@
 #include <cstdio>
 #include <sys/time.h>
 #include <math.h>
+#include <thread>
+#include <pthread.h>
 
 using namespace std;
 
-
-double timevalsub(struct timeval *tv1, const struct timeval *tv2) {
+double timevalsub(struct timeval *tv1, const struct timeval *tv2)
+{
   double res = 0;
   res = tv2->tv_sec - tv1->tv_sec;
-  res += (tv2->tv_usec - tv1->tv_usec)*1.0/1000000;
+  res += (tv2->tv_usec - tv1->tv_usec) * 1.0 / 1000000;
   return res;
 }
 
 // Returns the duration of call
-double fct(int num, int nbtours) {
+double fct(int num, int nbtours)
+{
   struct timeval tv1, tv2;
   int err;
 
   cout << num << " lancement de la fonction pour " << nbtours << " itérations"
-  << endl;
+       << endl;
   // #####################################
   // Partie calcul
   double temps_total = 0;
-  for (int i=0; i<nbtours; i++) {
+  for (int i = 0; i < nbtours; i++)
+  {
     int j;
-    
+
     err = gettimeofday(&tv1, NULL);
-    if (err != 0) {
+    if (err != 0)
+    {
       perror("gettimeofday");
       exit(EXIT_FAILURE);
     }
 
     double sum = 0;
-    for(j=0; j<5000000; j++) {
-      sum += log(j+1);
+    for (j = 0; j < 5000000; j++)
+    {
+      sum += log(j + 1);
     }
-    
+
     err = gettimeofday(&tv2, NULL);
-    if (err != 0) {
+    if (err != 0)
+    {
       perror("gettimeofday");
       exit(EXIT_FAILURE);
     }
@@ -52,7 +59,8 @@ double fct(int num, int nbtours) {
     //     << sum << ", il a fallu " << duree << " secondes" << endl ;
     // fprintf ne pose pas ce problème :
     fprintf(stdout, "%d : je viens de faire un calcul dont le résultat est %g,"
-	            "il a fallu %g secondes\n", num, sum, duree);
+                    "il a fallu %g secondes\n",
+            num, sum, duree);
   }
   // fin de la partie calcul
   // ###########################
@@ -60,32 +68,42 @@ double fct(int num, int nbtours) {
   return temps_total;
 }
 
+void calculTemps(int i, int nbtours, double &temps_total)
+{
+  double temps_fonction = fct(i, nbtours);
+  temps_total += temps_fonction;
+}
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   int i;
-  
+
   // Paramètres
   int nbthreads = 10;
   int nbtours = 5;
-  
+
   // lecture des arguments
-  if ((argc==2)&&(atoi(argv[1]))) {
+  if ((argc == 2) && (atoi(argv[1])))
+  {
     // s'il y a un argument numérique on l'utilise pour
     // connaître le nombre de threads/lancements de la fonction
     nbthreads = atoi(argv[1]);
-  } else if (argc != 1) {
+  }
+  else if (argc != 1)
+  {
     // il y a au moins un argument mais qui n'est pas le bon
     cerr << "usage " << argv[0] << " [<nb de lancements et de threads>]"
-	 << endl;
-    exit (1);
+         << endl;
+    exit(1);
   }
   cout << "Th principal : lancement de " << nbthreads << " fois la fonction"
        << endl;
-  
+
   double temps_total = 0;
-  for (i=0; i<nbthreads; i++) {
-    double temps_fonction = fct(i, nbtours);
-    temps_total += temps_fonction;
+  for (i = 0; i < nbthreads; i++)
+  {
+    thread *tmp = new thread(calculTemps, i, nbtours, ref(temps_total));
+    tmp->join();
   }
 
   cout << "Th principal : Le temps total de calcul est " << temps_total << endl;
